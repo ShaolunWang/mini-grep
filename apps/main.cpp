c037a91deef5d6b84bf87b0f933cc7dc21b003c5
me <wang.shaolun@gmail.com>
2026-03-22 15:41:25 +0800

    feat: add simple arg parsing

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,8 +1,14 @@
-#include "search/lib.h"
 #include "fmt/format.h"
+#include "search/lib.h"
 #include "search/policies.h"
-int main() {
-  Re2Matcher matcher("abcdef");
+int main(int argc, char **argv) {
+
+  if (argc < 2) {
+    fmt::println("Usage: {} <pattern>", argv[0]);
+    return 1;
+  }
+  const std::string pattern = argv[1];
+  Re2Matcher matcher(std::move(pattern));
   auto policy = std::make_unique<LockFreeSPSCPolicy>(matcher);
   //
   Engine<LockFreeSPSCPolicy> engine(std::move(policy));
